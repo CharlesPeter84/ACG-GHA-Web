005aa89b24b6555b6c16176f48330b4b7d093b78
CharlesPeter <charlesjpeter@gmail.com>
2026-06-13 16:44:10 +0530

    Allow web access to all

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,6 +37,40 @@ resource "aws_instance" "web" {
               systemctl start httpd
               echo "Hello from Terraform-deployed EC2" > /var/www/html/index.html
               EOF
-
   key_name = var.ssh_key_name != "" ? var.ssh_key_name : null
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and HTTPS inbound"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
