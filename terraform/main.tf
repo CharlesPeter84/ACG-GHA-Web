82a44172795af8e22a4fb5aa2245a1183ba36bdd
CharlesPeter <charlesjpeter@gmail.com>
2026-06-14 12:54:05 +0530

    Updated AMI search string

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "amazon_linux" {
   owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["al2023-ami-*-x86_64-gp3"]
+    values = ["al2023-ami-*-x86_64"]
   }
 }
 
